f834cc8a47431fb33b29033bcee7d00b61973382
kayoGc <kayivicti@gmail.com>
2023-06-01 20:17:50 -0300

    epa 2

---
+++ app/src/main/java/br/com/etecia/merecicla/CadastrarActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.merecicla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastrar_layout);
+    }
+}
--- app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
+++ app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
@@ -2,13 +2,24 @@ package br.com.etecia.merecicla;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView cadastrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        cadastrar = findViewById(R.id.txtcadastrar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),CadastrarActivity.class));
+            }
+        });
     }
 }
