e0680e034930634c2f6754459257ef1f9b0275af
kayoGc <kayivicti@gmail.com>
2023-06-01 21:30:24 -0300

    epa 3

--- app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
+++ app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     TextView cadastrar;
+    Button entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
         cadastrar = findViewById(R.id.txtcadastrar);
+        entrar = findViewById(R.id.btnentrar);
 
         cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
