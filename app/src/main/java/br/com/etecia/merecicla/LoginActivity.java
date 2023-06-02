fb6ef44d2856318a815e4324e1f765a604a70807
kayoGc <kayivicti@gmail.com>
2023-06-01 21:49:20 -0300

    epa 4

--- app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
+++ app/src/main/java/br/com/etecia/merecicla/LoginActivity.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
     TextView cadastrar;
+    TextInputEditText txtusuario, txtsenha;
     Button entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,26 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_layout);
         cadastrar = findViewById(R.id.txtcadastrar);
         entrar = findViewById(R.id.btnentrar);
+        txtusuario = findViewById(R.id.txtusuario);
+        txtsenha =  findViewById(R.id.txtsenha);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario, senha;
+
+                usuario = txtusuario.getText().toString();
+                senha = txtsenha.getText().toString();
+
+                if (usuario.equals("Mario") && senha.equals("Sonic") ){
+
+                    startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                }else {
+
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválidos, burro.",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         cadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +48,7 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),CadastrarActivity.class));
             }
         });
+
+
     }
 }
